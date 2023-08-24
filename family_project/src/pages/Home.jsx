6db612a9cb80f9174dc43d0373ea23cf1eed4711
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-08-24 20:21:04 +0200

    Image and styling added

--- family_project/src/pages/Home.jsx
+++ family_project/src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Nav from "../components/Nav";
+import heading from "../assets/images/myFonts/dr_mark_heading.png";
+import "/src/App.css";
 
 function Home() {
   return (
     <div>
-      <Nav />
+      <div>
+        <Nav />
+      </div>
+      <div className="flex justify-center bounce-in-right">
+        <img className="w-[975px] h-[480px]" src={heading} alt="Image" />
+      </div>
     </div>
   );
 }
