17715fbd1bf95465257160b154fe09aa27fd76db
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-08-24 19:19:32 +0200

    Nav added to Home

--- family_project/src/pages/Home.jsx
+++ family_project/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Nav from "../components/Nav";
 
 function Home() {
-  return <div>home</div>;
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
 }
 
 export default Home;
