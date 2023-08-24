8ca39b4fbaea3fada6aecadeddb87427623d0767
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-08-24 19:18:02 +0200

    Added routes for pages

--- family_project/src/App.jsx
+++ family_project/src/App.jsx
@@ -1,10 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home";
+import Story from "./pages/Story";
+import Gallery from "./pages/Gallery";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div>
-      <Home />{" "}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dr/story" element={<Story />} />
+        <Route path="/dr/gallery" element={<Gallery />} />
+        <Route path="/dr/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
